ee9144b5e9a0b3fd4722e638408a2063da791b92
Johan Alkstål <github@johanalkstal.com>
2025-04-09 15:59:12 +0200

    add theme select to dashboard

--- init.lua
+++ init.lua
@@ -199,6 +199,17 @@ require('lazy').setup({
 ██╔══██║██║     ██╔═██╗ ╚════██║   ██║   ██╔══██║██║     
 ██║  ██║███████╗██║  ██╗███████║   ██║   ██║  ██║███████╗
 ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝]],
+          ---@type snacks.dashboard.Item[]
+          keys = {
+            { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+            { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = '󱠓', key = 't', desc = 'Select Theme', action = ':Themery' },
+            { icon = ' ', key = 'c', desc = 'Config', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
+            { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+            { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+          },
           pick = 'telescope.nvim',
         },
         sections = {
