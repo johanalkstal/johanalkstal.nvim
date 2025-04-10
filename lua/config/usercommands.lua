f25e689f68af426c4069aad88ec516520776c24e
Johan Alkstål <github@johanalkstal.com>
2025-04-10 10:19:41 +0200

    add Dashboard command

--- lua/config/usercommands.lua
+++ lua/config/usercommands.lua
@@ -1,2 +1,7 @@
 -- Open the init.lua file.
 vim.api.nvim_create_user_command('Config', 'edit ~/.config/nvim/init.lua', {})
+
+-- Open the dashboard.
+vim.api.nvim_create_user_command('Dashboard', function()
+  require('snacks.dashboard').open()
+end, {})
