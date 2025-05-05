18e53c40d25bf50c655f6dbd180762d6364e06c1
Johan Alkstål <johan.alkstal@timetoact.at>
2025-05-05 17:18:52 +0200

    remove space

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -195,7 +195,7 @@ return {
               {
                 name = '@vue/typescript-plugin',
                 location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
-                languages = { 'vue ' },
+                languages = { 'vue' },
               },
             },
           },
