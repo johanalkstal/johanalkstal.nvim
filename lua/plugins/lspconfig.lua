55e9f2a1f8cb804414e21fa2bb4a7ece80096a5f
Johan Alkstål <johan.alkstal@timetoact.at>
2025-05-07 07:53:13 +0200

    fix keymap clash
    gr was mapped to both Go To References and Rename.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -71,7 +71,7 @@ return {
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
-          map('<leader>gr', vim.lsp.buf.rename, 'Rename')
+          map('<leader>gn', vim.lsp.buf.rename, 'Rename')
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
