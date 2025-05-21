33cdd97219a0a268940f58aa9c2836d1470ba5e0
Johan Alkstål <johan.alkstal@timetoact.at>
2025-05-21 12:15:47 +0200

    use icons for LSP files
    Makes the chosen LSP show up as an icon in the status bar which looks nicer.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,5 +1,5 @@
 -- Enable LSPs.
-vim.lsp.enable { 'astro', 'gleam', 'typescript' }
+vim.lsp.enable { '', 'gleam', '' }
 
 vim.api.nvim_create_autocmd('LspAttach', {
   -- Groups helps manage these LSP-related autocommands separately from other autocommands you might have.
